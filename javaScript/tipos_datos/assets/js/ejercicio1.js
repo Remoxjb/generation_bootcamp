41b2089b3c28c0a008efe113082f23a535978441
Juan <juan.badillo.v@gmail.com>
2022-04-26 14:41:43 -0500

    sesion 26 abril

---
+++ javaScript/tipos_datos/assets/js/ejercicio1.js
@@ -0,0 +1,8 @@
+const anoNacimiento = prompt ("Ingresa tu año de nacimiento");
+
+console.log(anoNacimiento);
+
+if (true){
+    const edad = 2022 - anoNacimiento
+    console.log("tu edad es de:", edad, "años");
+}
---
+++ javaScript/tipos_datos/assets/js/tiposDatos.js
@@ -0,0 +1,27 @@
+let nombre = "Juan"; // es un string
+let edad = 30 // es un number
+
+const gravedad = 9.81 //const no se puede reasignar un valor
+
+let dinero = 23.3  //es un número flotante
+let tinesTrabajo = true; // es un boolean
+let variableNull = null; //es una variable con tipo de dato nula
+
+let varaiableIndefinida; // no se define con ningun valor
+
+console.log(typeof(varaiableIndefinida));
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tinesTrabajo));
+console.log(typeof(variableNull));
+
+/* var let y const */
+
+/* if (true){
+    let numero = 10 //cuando poner var aunque este dentro de una variable aunque este dentro de una variable se puede utilizar despues pero VAR ya no se ocupa mucho debido a que puede usar esa variable
+     console.log(numero);
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i);
+} */
